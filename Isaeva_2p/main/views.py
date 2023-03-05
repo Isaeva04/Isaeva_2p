89042d3aceba403ae9c9645f15ab6095dab06f67
-ASUS- <Isaevaa@tut.by>
2023-03-06 00:00:18 +0300

    Isaeva2p

--- Isaeva_2p/main/views.py
+++ Isaeva_2p/main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse # если текст страницы прописывать ниже, без файлов штмл
 
 def index (request):
-    return HttpResponse('<h4>Hello</h4>')
+    return render(request, 'index.html')
+
 def about (request):
-    return HttpResponse('<h4>About</h4>')
+    return render(request, 'about.html')
