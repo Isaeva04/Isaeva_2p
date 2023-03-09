143c8ef0eecf4d719148c74f5d4c36ba75e554bf
-ASUS- <Isaevaa@tut.by>
2023-03-09 19:51:55 +0300

    Isaeva2p

--- Isaeva_2p/main/views.py
+++ Isaeva_2p/main/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse # если текст страницы прописывать ниже, без файлов штмл
+from .models import Task
 
-def index (request):
-    return render(request, 'index.html')
+def index(request):
+    task = Task.objects.order_by('-id')
+    return render(request, 'index.html', {'title': 'Главная страница', 'tasks': task})
 
-def about (request):
+
+def about(request):
     return render(request, 'about.html')
+
+
+def create(request):
+    return render(request, 'create.html')
